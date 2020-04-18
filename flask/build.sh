4a2e3a750f4951e215e2543585c5d61e119cfb5b
sunpm6 <51981748+sunpm6@users.noreply.github.com>
2020-04-19 04:53:48 +0530

    Update build.sh

--- flask/build.sh
+++ flask/build.sh
@@ -4,4 +4,5 @@ echo "********************************"
 cd flask
 docker build -t api:v1 .
 echo " "
+curl -X POST --data-urlencode "payload={\"channel\": \"#alerts-grafana\", \"username\": \"sunil-api\", \"text\": \"Docker build Successfull image=api:v1.\n $ac \", \"icon_emoji\": \":fire:\"}" https://hooks.slack.com/services/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
 echo "Build Succeded!!"
