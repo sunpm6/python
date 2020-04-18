6fe0efbcb419d526a52416e78a3d3da56601def2
sunpm6 <51981748+sunpm6@users.noreply.github.com>
2020-04-19 03:22:56 +0530

    Update build.sh

--- flask/build.sh
+++ flask/build.sh
@@ -1,6 +1,6 @@
 echo "*********************************"
 echo "*****Building the Image*********"
 echo "********************************"
-docker build -t api:v1 .
+docker build -t api:v1 .flask/
 echo " "
 echo "Build Succeded!!"
