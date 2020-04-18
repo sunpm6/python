b5fc4575c91e0e42e1df4b343c2a53c1ea1aa6e6
sunpm6 <pm.sunil692@gmail.com>
2020-04-19 00:45:48 +0530

    First commit

---
+++ flask/build.sh
@@ -0,0 +1,6 @@
+echo "*********************************"
+echo "*****Building the Image*********"
+echo "********************************"
+docker build -t api:v1 .
+echo " "
+echo "Build Succeded!!"
