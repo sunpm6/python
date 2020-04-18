e4d52024b1f7fd674495ff25f898036732a662ef
sunpm6 <51981748+sunpm6@users.noreply.github.com>
2020-04-19 03:43:11 +0530

    Update deploy.sh

--- flask/deploy.sh
+++ flask/deploy.sh
@@ -1,6 +1,6 @@
 echo "***********************************"
 echo "*****Deploying the Image*********"
 echo "**********************************"
-docker run --name api-python1$BUILD_NUMBER -p 8081:5000 -d api:v1
+docker run --name api-python1$BUILD_NUMBER -p 8081:8082 -d api:v1
 echo " "
 echo "Build Succeded!!"
